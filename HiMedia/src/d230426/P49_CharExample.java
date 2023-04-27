e2b58d537cbb470034db40cd9c4255baf255fa00
Class01 <Class01@DESKTOP-AHAB51C>
2023-04-27 14:51:19 +0900

    230426

---
+++ HiMedia/src/d230426/P49_CharExample.java
@@ -0,0 +1,13 @@
+package d230426;
+
+public class P49_CharExample {
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 65;
+		int i3 = 65;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(i3);
+	}
+}
---
+++ HiMedia/src/d230426/P53_LongExample.java
@@ -0,0 +1,17 @@
+package d230426;
+
+public class P53_LongExample {
+
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 200L;
+//		long var3 = 100000000000000;
+		long var4 = 100000000000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+//		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
