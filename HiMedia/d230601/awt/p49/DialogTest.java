89bda512bd827422b37bfcc0c6357c7f8aff3b5a
Class01 <Class01@DESKTOP-AHAB51C>
2023-06-01 18:04:23 +0900

    close 추가

--- HiMedia/d230601/awt/p49/DialogTest.java
+++ HiMedia/d230601/awt/p49/DialogTest.java
@@ -5,38 +5,58 @@ import java.awt.Dialog;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
- 
-public class DialogTest extends WindowAdapter{
+
+public class DialogTest extends WindowAdapter implements ActionListener {
 	private Frame f;
 	private Dialog info;
+	private Label msg;
+	private Button ok;
+
+	public DialogTest() {
+		f = new Frame("Parent");
+		info = new Dialog(f, "Information", true);
+		msg = new Label("This is modal Dialog", Label.CENTER);
+		ok = new Button("OK");
+	}
 	
+	public void startFrame() {
+		f.setSize(300, 200);
+		info.setSize(150, 100);
+		info.setLocation(75, 50);
+		info.setLayout(new FlowLayout());
+		info.addWindowListener(this);
+		info.add(msg);
+		info.add(ok);
+
+		f.addWindowListener(this);
+		ok.addActionListener(this);
+		info.addWindowListener(this);
+		
+		f.setVisible(true);
+		info.setVisible(true);
+	}
+
 	public void windowClosing(WindowEvent e) {
 		info.dispose();
+		if (e.getComponent() == info) {
+			info.dispose();
+		} else {
+			System.exit(0);
+		}
 	}
-	
-	public DialogTest() {
-        f = new Frame("Parent");
-        f.setSize(300, 200);
- 
-        info = new Dialog(f, "Information", true);
-        info.setSize(150, 100);
-        info.setLocation(75, 50);
-        info.setLayout(new FlowLayout());
-        info.addWindowListener(this);
- 
-        Label msg = new Label("This is modal Dialog", Label.CENTER);
-        Button ok = new Button("OK");
-        info.add(msg);
-        info.add(ok);
- 
-        f.setVisible(true);
-        info.setVisible(true);
-	}
-	
-    public static void main(String[] args) {
-    	DialogTest d = new DialogTest();
-    }
-}
 
+	public static void main(String[] args) {
+		DialogTest d = new DialogTest();
+		d.startFrame();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		info.dispose();
+	}
+}
